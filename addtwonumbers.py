b450d6c7e69828286f952ecb6a48b33a6dcfc30e
endar-firmansyah <mailuntukbelajar@gmail.com>
2020-12-11 14:09:10 +0700

    Add Two Numbers code

---
+++ addtwonumbers.py
@@ -0,0 +1,18 @@
+num1 = 2.3
+num2 = 4.2
+
+num3 = num1 + num2
+print('Hasilnya pertambahan {0} dan {1}  adalah: {2}'.format(num1,num2,num3))
+
+
+firstnum = input('Enter First Number: ')
+secnum = input('Enter Second Number: ')
+
+sum = float(firstnum) + float(secnum)
+print(f'The result is:', (sum))
+
+
+print ('The sum is %.1f' %(float(input('Enter First Number: ')) + float(input('The Second Number: '))))
+
+
+
