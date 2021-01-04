83af4b2f3aea4751e7ee505c1e9cc7ffab29ec24
endar-firmansyah <mailuntukbelajar@gmail.com>
2021-01-04 11:19:39 +0700

    Cenverting Decimal into other numbers

---
+++ convdecimal.py
@@ -0,0 +1,6 @@
+# Python program to convert Decimal into other number systems
+dec = int(input("Enter Decimal number: "))
+
+print(bin(dec), "in Binary.")
+print(oct(dec), "in Octal")
+print(hex(dec), "in Hexadecimal.")
